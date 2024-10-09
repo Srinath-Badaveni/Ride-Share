1641f69f4733e8c3f8fb54837bbc8f8c87c68709
Srinath-Badaveni <badavenisrinath@gmail.com>
2024-10-09 12:03:26 +0530

    auto delete rides

--- controllers/route.js
+++ controllers/route.js
@@ -1,20 +1,40 @@
 const Route = require("../models/route");
 
 module.exports.index = async (req, res) => {
-  const Routes = await Route.find({});
+  const rides = await Route.find({});
+  let currentDate = new Date();
+  const Routes = rides.filter(async (ride) => {
+    if (ride.date < currentDate) {
+      return await Route.findByIdAndDelete(ride._id);
+    } else {
+      return ride.date > currentDate;
+    }
+  });
+
   res.render("home/main.ejs", { Routes });
 };
 
 module.exports.addNew = async (req, res) => {
   let sampleRoute = new Route(req.body.route);
+  let currentDate = new Date();
+  let maxDate = new Date();
+  maxDate.setDate(currentDate.getDate() + 5);
+  if (sampleRoute.date > maxDate) {
+    req.flash("error", "Date Should be in 5 days form today !!");
+    return res.redirect("/route/addNew");
+  }
   sampleRoute.owner = req.user._id;
   await sampleRoute.save();
   res.redirect("/route");
-}
+};
 
 module.exports.renderNew = (req, res) => {
-  res.render("home/addRoute.ejs");
-}
+  let currentDate = new Date();
+  let maxDate = new Date();
+  maxDate.setDate(currentDate.getDate() + 5);
+  console.log(maxDate);
+  res.render("home/addRoute.ejs", { maxDate });
+};
 
 module.exports.renderEditForm = async (req, res) => {
   const { id } = req.params;
@@ -23,32 +43,32 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("error", "Listing not found ");
     res.redirect("/route");
   }
-  res.render("home/editRoute.ejs", {ride});
-}
+  res.render("home/editRoute.ejs", { ride });
+};
 
 module.exports.editRoute = async (req, res) => {
   const { id } = req.params;
   const ride = await Route.findOneAndUpdate({ _id: id }, { ...req.body.route });
   req.flash("success", "Route Edited successfully!!");
   res.redirect(`/route`);
-}
+};
 
-module.exports.myRides = async(req,res)=>{
-  const {id} = req.params;
-  const Routes = await Route.find({owner: id}).populate("owner")
-  res.render("home/myRides.ejs",{Routes})
-}
+module.exports.myRides = async (req, res) => {
+  const { id } = req.params;
+  const Routes = await Route.find({ owner: id }).populate("owner");
+  res.render("home/myRides.ejs", { Routes });
+};
 
-module.exports.RideDetails = async(req,res)=>{
-  const{id} = req.params
-  const ride = await Route.findById(id).populate("bookings")
-  console.log(ride)
-  res.render("home/rideDetails.ejs",{ride})
-}
+module.exports.RideDetails = async (req, res) => {
+  const { id } = req.params;
+  const ride = await Route.findById(id).populate("bookings");
+  console.log(ride);
+  res.render("home/rideDetails.ejs", { ride });
+};
 
-module.exports.deleteRide = async(req,res)=>{
-  const{id} = req.params;
-  const ride = await Route.findByIdAndDelete(id)
-  req.flash("success","route deleted successfully")
-  res.redirect(`/route/${req.user._id}`)
-}
+module.exports.deleteRide = async (req, res) => {
+  const { id } = req.params;
+  const ride = await Route.findByIdAndDelete(id);
+  req.flash("success", "route deleted successfully");
+  res.redirect(`/route/${req.user._id}`);
+};
--- routes/book-now.js
+++ routes/book-now.js
@@ -2,14 +2,14 @@ const express = require("express");
 const wrapasync = require("../utils/wrapasync");
 const router = express.Router({ mergeParams: true });
 const passport = require("passport");
-const { saveUrl, isLoggedIn } = require("../middleware");
+const { saveUrl, isLoggedIn, checkExpiration } = require("../middleware");
 
 const bookNowController = require("../controllers/book-now");
 
 router
   .route("/")
-  .get(isLoggedIn,saveUrl, wrapasync(bookNowController.renderBookNow))
-  .post(isLoggedIn,saveUrl, wrapasync(bookNowController.conformBooking));
+  .get(checkExpiration,isLoggedIn,saveUrl, wrapasync(bookNowController.renderBookNow))
+  .post(checkExpiration,isLoggedIn,saveUrl, wrapasync(bookNowController.conformBooking));
 router.route('/conform').post(bookNowController.booking)
 
 router.route('/delete/:bookingId').get(wrapasync(bookNowController.deleteBooking))
